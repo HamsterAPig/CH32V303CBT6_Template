09e6501b5fbe69054ce36f18c9b17570631d3bad
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2024-06-17 22:35:52 +0800

    :art: (clang-format): Init

--- User/main.cpp
+++ User/main.cpp
@@ -1,14 +1,14 @@
 /********************************** (C) COPYRIGHT *******************************
-* File Name          : main.c
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2021/06/06
-* Description        : Main program body.
-*********************************************************************************
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* Attention: This software (modified or not) and binary are used for 
-* microcontroller manufactured by Nanjing Qinheng Microelectronics.
-*******************************************************************************/
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2021/06/06
+ * Description        : Main program body.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
 
 /*
  *@Note
@@ -20,13 +20,11 @@
 
 #include "debug.h"
 
-
 /* Global typedef */
 
 /* Global define */
 
 /* Global Variable */
-
 
 /*********************************************************************
  * @fn      main
@@ -40,14 +38,12 @@ int main(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	SystemCoreClockUpdate();
 	Delay_Init();
-	USART_Printf_Init(115200);	
-	printf("SystemClk:%lu\r\n",SystemCoreClock);
-	printf( "ChipID:%08lx\r\n", DBGMCU_GetCHIPID() );
+	USART_Printf_Init(115200);
+	printf("SystemClk:%lu\r\n", SystemCoreClock);
+	printf("ChipID:%08lx\r\n", DBGMCU_GetCHIPID());
 	printf("This is printf example\r\n");
 
-	while(1)
-    {
-
+	while (1)
+	{
 	}
 }
-
